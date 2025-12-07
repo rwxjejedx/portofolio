fbc2f5b3133e54c733dbc7d095480a51a7ed097d
maulanajulham <julhammaulana@gmail.com>
2025-12-07 07:07:11 +0700

    feat:fix navbar

--- my-portofolio-apps/src/components/Navbar.tsx
+++ my-portofolio-apps/src/components/Navbar.tsx
@@ -20,23 +20,22 @@ const Navbar = () => {
     <div className="navbar py-7 w-full flex justify-center">
         <ul 
             className={`
-                menu flex justify-between gap-4 text-white
+                menu flex justify-between gap-3 md:gap-15 text-white
+                text-xs w-full  fixed left-1/2 -translate-x-1/2 z-33
                 
-                // Lebar dan Posisi Default (Mobile)
-                w-11/12 sm:w-10/12 // Gunakan w-11/12 atau w-full untuk mengatasi masalah 
-                fixed left-1/2 -translate-x-1/2
                 
                 // Lebar dan Posisi MD (Desktop)
                 md:static md:w-auto md:max-w-5xl md:-translate-x-0
                 
-                md:opacity-100 bg-white/30 backdrop-blur-md p-4 rounded-2xl 
+                md:opacity-100 bg-white/20 backdrop-blur-md p-4 rounded-2xl 
                 transition-all md:transition-none 
                 ${active ? "top-5 opacity-100" : "-top-3"}
             `}
         >
             <li><a href="#" className="sm:text-lg text-base font-medium">Home</a></li>
-            <li><a href="#" className="sm:text-lg text-base font-medium">Project</a></li>
             <li><a href="#" className="sm:text-lg text-base font-medium">About</a></li>
+            <li><a href="#" className="sm:text-lg text-base font-medium">Tools</a></li>
+            <li><a href="#" className="sm:text-lg text-base font-medium">Portofolio</a></li>
             <li><a href="#" className="sm:text-lg text-base font-medium">Contact</a></li>
         </ul>
     </div>
